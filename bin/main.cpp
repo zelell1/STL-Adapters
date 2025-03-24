01cb0f5060c2b411d2739d89a1a8f749735b5f86
zelell1 <lazimovdi@gmail.com>
2025-03-24 16:12:29 +0300

    v1.0

--- bin/main.cpp
+++ bin/main.cpp
@@ -8,27 +8,27 @@
 */
 
 int main(int argc, char **argv) {
-  if(agrc != 2)
+  if(argc != 2)
     return 1;
   
 
   bool recursive = false;
-  Dir(argv[1], recursive) 
-    | Filter([](std::filesystem::path& p){ return p.extension() == ".txt"; })
-    | OpenFiles()
-    | Split("\n ,.;")
-    | Transform(
-        [](std::string& token) { 
-            std::transform(token.begin(), token.end(), [](char c){return std::tolower(c);});
-            return token;
-        })
-    | AggregateByKey(
-        0uz, 
-        [](const std::string&, size_t& count) { ++count;},
-        [](const std::string& token) { return token;}
-      )
-    | Transform([](const std::pair<std::string, size_t>& stat) { return std::format("{} - {}", stat.first, stat.second);})
-    | Out(std::cout);
+  // Dir(argv[1], recursive) 
+  //   | Filter([](std::filesystem::path& p){ return p.extension() == ".txt"; })
+  //   | OpenFiles()
+  //   | Split("\n ,.;")
+  //   | Transform(
+  //       [](std::string& token) { 
+  //           std::transform(token.begin(), token.end(), [](char c){return std::tolower(c);});
+  //           return token;
+  //       })
+  //   | AggregateByKey(
+  //       0uz, 
+  //       [](const std::string&, size_t& count) { ++count;},
+  //       [](const std::string& token) { return token;}
+  //     )
+  //   | Transform([](const std::pair<std::string, size_t>& stat) { return std::format("{} - {}", stat.first, stat.second);})
+  //   | Out(std::cout);
 
   
   return 0;
